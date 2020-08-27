c548c0feedd588d3169f25e80bc1bac417cce1af
Dev-Bhojak <devbhojak1@outlook.com>
2020-08-27 11:51:55 +1000

    commit

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+const { response, json } = require('express');
+
+const app = new Clarifai.App({
+    apiKey: '50a67b2be9284254adcfe38ed5b2bd6c'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
